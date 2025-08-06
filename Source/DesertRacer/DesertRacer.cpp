64560786e853712f46b599421c03b31553264185
Mohammad Babaei <mohammadhosseinb80@gmail.com>
2025-08-06 06:08:37 +0330

    remove project

--- Source/DesertRacer/DesertRacer.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DesertRacer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DesertRacer, "DesertRacer" );
